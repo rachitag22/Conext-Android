799b965eb8722f20b3e69adb873b65c2e3018751
rachitag22 <redskins222@gmail.com>
2016-04-26 12:45:12 -0400

    aslkdfj

---
+++ app/build/generated/source/buildConfig/debug/com/rachitagarwal/conext/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rachitagarwal.conext;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.rachitagarwal.conext";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
